819e6cf58c4be8bbe70d7b7982cbef7b35ca20b2
elliott-rosenberg <elliott_rosenberg@brown.edu>
2023-12-02 15:18:56 -0500

    day 1

--- code/01.py
+++ code/01.py
@@ -23,7 +23,7 @@ def solvePart1(file):
 def solvePart2(file):
     calibration_sum = 0
     for row in file:
-        matched_row = re.findall(r'(one|two|three|four|five|six|seven|eight|nine|\d)', row)
+        matched_row = re.findall(r'(zero|one|two|three|four|five|six|seven|eight|nine|\d)', row)
 
 
         first_num = matched_row[0] if len(matched_row[0]) == 1 else word_to_num.get(matched_row[0])
@@ -60,3 +60,34 @@ def solve(file):
     print(f"Part 2: {solvePart2(processed)}")
 
 solve("inputs/01/input1.txt")
+
+
+nums = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+}
+def get_digit(c: str, line: str, i: int) -> str:
+    if c.isdigit() and c != '0': return c 
+    for j in range(3, 6):
+        if line[i:i+j] in nums: return nums[line[i:i+j]]
+    return ''
+with open('inputs/01/input1.txt') as f: file = f.read()
+lines = file.split('\n')
+ans = 0
+for line in lines:
+    first, last, i = 0, 0, 0
+    for c in line:
+        d = get_digit(c, line, i)
+        if d != '':
+            last = d
+            if first == 0: first = d
+        i += 1
+    ans += int(first + last)
+print(ans)
