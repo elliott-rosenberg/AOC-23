95cab2109053e1803237bf91f6e7039d457ecd9c
elliott-rosenberg <elliott_rosenberg@brown.edu>
2023-12-04 11:41:38 -0500

    day 4

--- code/03.py
+++ code/03.py
@@ -1,8 +1,97 @@
+import re
+
 def solvePart1(file):
-    return None
+    parts_sum = 0
+    engine_array = []
+    amt = len(file[0]) + 2
+    first = ['.' for _ in range(amt)]
+    engine_array.append(first)
+    for row in file:
+        engine_row = []
+        engine_row.append('.')
+        [engine_row.append(l) for l in row]
+        engine_row.append('.')
+        engine_array.append(engine_row)
+    engine_array.append(first)
+    for rowIndex, row in enumerate(engine_array):
+        # print(row)
+        numStart = False
+        num = ''
+        for colIndex, entry in enumerate(row):
+            if entry.isdigit():
+                numStart = True
+                num = num + entry
+            if (not entry.isdigit()) and numStart:
+                numStart = False
+                isPart = checkPart(engine_array, rowIndex, colIndex, len(num))
+                if isPart:
+                    parts_sum += int(num)
+                num = ''
+                #if at this point we can check for symbols in the previous spots
+    return parts_sum
+        
+
+        
+
+
+def checkPart(engArray, row, col, numLen) -> bool:
+    for rowInd in range(row-1, row+2):
+        for colInd in range(col - numLen - 1, col + 1):
+            if len(re.findall('[^.\d]', engArray[rowInd][colInd])) > 0:
+                return True
+    return False
 
 def solvePart2(file):
-    return None
+    ratio_sum = 0
+    engine_array = []
+    amt = len(file[0]) + 2
+    first = ['.' for _ in range(amt)]
+    engine_array.append(first)
+    for row in file:
+        engine_row = []
+        engine_row.append('.')
+        [engine_row.append(l) for l in row]
+        engine_row.append('.')
+        engine_array.append(engine_row)
+    engine_array.append(first)
+    for rowIndex, row in enumerate(engine_array):
+        # print(row)
+        numStart = False
+        num = ''
+        for colIndex, entry in enumerate(row):
+            if entry.isdigit():
+                numStart = True
+                num = num + entry
+            if (not entry.isdigit()) and numStart:
+                numStart = False
+                isPart = checkRatio(engine_array, rowIndex, colIndex, len(num), int(num))
+                num = ''
+                #if at this point we can check for symbols in the previous spots
+    print(num_map)
+    for val in num_map.values():
+        if len(val) == 2:
+            ratio_sum += val[0] * val[1]
+    return ratio_sum
+
+num_map = {}
+
+def checkRatio(engArray, row, col, numLen, num) -> bool:
+    for rowInd in range(row-1, row+2):
+        for colInd in range(col - numLen - 1, col + 1):
+            if engArray[rowInd][colInd] == '*':
+                print(num)
+                key = f"{rowInd} {colInd}"
+                if not key in num_map:
+                    num_map[key] = [num]
+                else:
+                    val = num_map[key]
+                    val.append(num)
+                    num_map[key] = val
+
+            if len(re.findall('[^.\d]', engArray[rowInd][colInd])) > 0:
+                return True
+    return False
+
 
 def process(file):
     rows = [l.strip() for l in open(file).readlines()]
--- code/04.py
+++ code/04.py
@@ -1,8 +1,62 @@
+import re
+
 def solvePart1(file):
-    return None
+    match_sum = 0
+    for row in file:
+        splitted = row.split('|')
+        winning_nums = splitted[0].split(':')[1].strip().split(' ')
+        for l in winning_nums:
+            if l == '':
+                winning_nums.remove(l)
+        card_nums = splitted[1].strip().split(' ')
+        for l in card_nums:
+            if l == '':
+                card_nums.remove(l)
+        # print(winning_nums)
+        # print(card_nums)
+
+        matches = 0
+        for num in winning_nums:
+            for c_num in card_nums:
+                if int(num) == int(c_num):
+                    matches += 1
+        if not matches == 0:
+            match_sum += 2 ** (matches - 1)
+                        
+
+                        
+    return match_sum
 
 def solvePart2(file):
-    return None
+    card_total = 0
+    card_copies = [1 for _ in range(len(file))]
+    for row in file:
+        
+        splitted = row.split('|')
+        winning_nums = splitted[0].split(':')[1].strip().split(' ')
+        card_number = int(re.findall('\d+', splitted[0].split(':')[0])[0])
+        print(card_number)
+        card_multiplier = card_copies[card_number-1]
+        for l in winning_nums:
+            if l == '':
+                winning_nums.remove(l)
+        card_nums = splitted[1].strip().split(' ')
+        for l in card_nums:
+            if l == '':
+                card_nums.remove(l)
+
+        matches = 0
+        for num in winning_nums:
+            for c_num in card_nums:
+                if int(num) == int(c_num):
+                    matches += 1
+        for card in range(card_number, card_number + matches):
+            card_copies[card] += card_multiplier
+        card_total += card_copies[card_number-1]
+
+
+        
+    return card_total
 
 def process(file):
     rows = [l.strip() for l in open(file).readlines()]
