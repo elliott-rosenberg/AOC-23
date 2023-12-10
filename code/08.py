3405b26e35cbfb3472a8eea7f2a2c9023fafb557
elliott-rosenberg <elliott_rosenberg@brown.edu>
2023-12-10 15:24:09 -0500

    day9

--- code/08.py
+++ code/08.py
@@ -1,16 +1,157 @@
-def solvePart1(file):
-    return None
+# def solvePart1(file):
+#     firstRow = True
+#     dirs = ''
+#     coords_map = {}
+#     for row in file:
+#         # print(row)
+#         if firstRow:
+#             firstRow = False
+#             dirs = row
+#         else :
+#             if len(row) > 1:
 
-def solvePart2(file):
-    return None
+#                 source = row.split('=')[0].strip()
+#                 dest1 = row.split('=')[1].strip().split(',')[0][1:]
+#                 print(dest1)
+#                 dest2 = row.split('=')[1].strip().split(',')[1][1:-1]
+#                 coords_map[source] = (dest1, dest2)
+#     # print(coords_map)
+#     zSteps = 0
+#     reachedZ = False
+#     dirIndex = 0
+#     s = 'AAA'
+#     while (not reachedZ):
+#         zSteps += 1
+#         direction = dirs[dirIndex % len(dirs)]
+#         dirIndex += 1
 
-def process(file):
-    rows = [l.strip() for l in open(file).readlines()]
-    return rows
+#         if (direction == 'L'):
+#             s = coords_map[s][0]
+#         else:
+#             s = coords_map[s][1]
+#         if s == 'ZZZ':
+#             break
+        
 
-def solve(file):
-    processed = process(file)
-    print(f"Part 1: {solvePart1(processed)}")
-    print(f"Part 2: {solvePart2(processed)}")
+#     return zSteps
 
-solve("inputs/08/input1.txt")
+# def solvePart2(file):
+#     firstRow = True
+#     dirs = ''
+#     coords_map = {}
+#     for row in file:
+#         if firstRow:
+#             firstRow = False
+#             dirs = row
+#         else :
+#             if len(row) > 1:
+
+#                 source = row.split('=')[0].strip()
+#                 dest1 = row.split('=')[1].strip().split(',')[0][1:]
+#                 dest2 = row.split('=')[1].strip().split(',')[1][1:-1]
+#                 coords_map[source] = (dest1, dest2)
+#     sources = []
+#     for coord in coords_map.keys():
+#         if coord[-1] == 'A':
+#             sources.append(coord)
+
+    
+#     zSteps = 0
+#     reachedZ = False
+#     dirIndex = 0
+#     while (not reachedZ):
+#         zSteps += 1
+#         direction = dirs[dirIndex % len(dirs)]
+#         dirIndex += 1
+
+#         for index, so in enumerate(sources):
+#             if (direction == 'L'):
+#                 sources[index] = coords_map[so][0]
+#             else:
+#                 sources[index] = coords_map[so][1]
+#         if all(s[-1] == 'Z' for s in sources):
+#             break
+
+
+
+#     return zSteps
+
+# def process(file):
+#     rows = [l.strip() for l in open(file).readlines()]
+#     return rows
+
+# def solve(file):
+#     processed = process(file)
+#     #print(f"Part 1: {solvePart1(processed)}")
+#     print(f"Part 2: {solvePart2(processed)}")
+
+# solve("inputs/08/input1.txt")
+
+
+import re
+import math
+
+class Leaf:
+    def __init__(self, l, r):
+        self.left = l
+        self.right = r
+
+    def __repr__(self) -> str:
+        return f"({self.left}, {self.right})"
+
+def part_1(instructions, tree):
+    current_match = "AAA"
+    total_iteration = 0
+    iteration = 0
+    while current_match != "ZZZ":
+        if instructions[iteration] == "R":
+            current_match = tree[current_match].right
+        if instructions[iteration] == "L":
+            current_match = tree[current_match].left
+
+        total_iteration += 1
+        iteration = (iteration + 1) % len(instructions)
+        
+    print(f"Part 1: {total_iteration}")
+
+def part_2(instructions, tree):
+    current_match_list = [key for key in tree.keys() if key.endswith('A')]
+
+    total_iteration = 0
+    iteration = 0
+    while not all(isinstance(element, int) for element in current_match_list):
+        for idx in range(len(current_match_list)):
+            element = current_match_list[idx]
+            if isinstance(element, int): continue
+            if element.endswith('Z'):
+                current_match_list[idx] = int(total_iteration)
+                continue
+
+            if instructions[iteration] == "R":
+                element = tree[element].right
+            if instructions[iteration] == "L":
+                element = tree[element].left
+
+            current_match_list[idx] = element
+
+        total_iteration += 1
+        iteration = (iteration + 1) % len(instructions)
+
+    result = math.lcm(*current_match_list)
+    print(f"Part 2: {result}")
+
+with open("inputs/08/input1.txt") as data:
+    lines = data.readlines()
+    
+    instructions = lines[0][0:-1]
+    tree_lines = lines[2:]
+
+    tree = {}
+    pattern = re.compile(r'(\w+)\s*=\s*\((\w+),\s*(\w+)\)')
+    for entry in tree_lines:
+        match = pattern.match(entry)
+        tree[match.group(1)] = Leaf(match.group(2), match.group(3))
+    
+    # Algorithm
+    part_1(instructions, tree)
+    part_2(instructions, tree)
--- code/09.py
+++ code/09.py
@@ -1,8 +1,56 @@
 def solvePart1(file):
-    return None
+    history_sum = 0
+    values = []
+    for row in file:
+        values.append([int(l) for l in row.split(' ')])
+    for v in values:
+        # print(f"value: {v}")
+        history_sum += getDif([v[-1]], v)
+    return history_sum
+
+
+
+def getDif(lastVals, inList) -> int:
+    nextList = []
+    for i in range(len(inList)-1):
+        nextList.append(inList[i+1] - inList[i])
+    lastVals.append(nextList[-1])
+    if len(set(nextList)) == 1:
+        # print(f"next input: {nextList}")
+        # print(f"lastVals: {lastVals}")
+        return sum(lastVals)
+    else:
+        return getDif(lastVals, nextList)
+
+def getSecondDif(lastVals, inList) -> int:
+    nextList = []
+    for i in range(len(inList)-1):
+        nextList.append(inList[i+1] - inList[i])
+    lastVals.append(nextList[0])
+    #print(f"next input: {nextList}")
+    if all(ele == 0 for ele in nextList):
+        out = 0
+        for i in range(len(lastVals), 0, -1):
+            out = lastVals[i-1] - out
+        #print(f"lastVals: {lastVals}")
+        return out
+    else:
+        return getSecondDif(lastVals, nextList)
+
+
+
+
+
 
 def solvePart2(file):
-    return None
+    history_sum = 0
+    values = []
+    for row in file:
+        values.append([int(l) for l in row.split(' ')])
+    for v in values:
+        #print(f"value: {v}")
+        history_sum += getSecondDif([v[0]], v)
+    return history_sum
 
 def process(file):
     rows = [l.strip() for l in open(file).readlines()]
@@ -12,5 +60,7 @@ def solve(file):
     processed = process(file)
     print(f"Part 1: {solvePart1(processed)}")
     print(f"Part 2: {solvePart2(processed)}")
+
+
 
 solve("inputs/09/input1.txt")
